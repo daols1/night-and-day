6565ba959eed95018ed6d1eb70e8b1a967c3ac78
David <78326757+daols1@users.noreply.github.com>
2022-07-12 17:55:17 +0100

    Done +1:

---
+++ main.js
@@ -0,0 +1,10 @@
+// const btn1 = document.querySelector('.night');
+// const btn2 = document.querySelector('.day');
+// btn1.addEventListener('click', () => {
+//     btn1.classList.remove('night')
+//     btn1.classList.add('day')
+// })
+// btn1.addEventListener('click', () => {
+//     btn2.classList.remove('day')
+//     btn2.classList.add('night')
+// })
